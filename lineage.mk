b63bc62cdec6066d924efa79de8f7b181572eae0
vedant360 <31750922+vedant360@users.noreply.github.com>
2017-11-16 21:13:57 +0530

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, device/lge/c70n/full_c70n.mk)
 
