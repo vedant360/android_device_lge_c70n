0c13b6d7fb3806ccc92a9020d1df820f37d43778
vedant360 <31750922+vedant360@users.noreply.github.com>
2017-11-20 22:21:40 +0530

    Update and rename lineage.mk to aosp_c70n.mk

--- aosp_c70n.mk
+++ aosp_c70n.mk
@@ -15,8 +15,7 @@
 #
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/aosp/common.mk)
